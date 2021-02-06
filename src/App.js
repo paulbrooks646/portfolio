4169d12100be7027b07cebde08a7ef8f5338f537
paulbrooks646 <paulbrooks646@gmail.com>
2021-02-06 11:41:47 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="portfolio-main">
+      <div className="portfolio-introduction">
+        <h1 className="portfolio-title">Paul Brooks</h1>
+        <h2 className="portfolio-subtitle">Web Developer</h2>
+      </div>
+      <div className="portfolio-projects">
+        <h3>PROJECTS:</h3>
+        <div className="simpsons"></div>
+        <div className="disneyland"></div>
+        <div className="css"></div>
+        <div className="codepound"></div>
+      </div>
+      <div className="portfolio-contact">
+        
+      </div>
     </div>
   );
 }
