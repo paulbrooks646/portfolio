1b7a4bfc52706c249d72aa80ec2365aa8a3cfeac
paulbrooks646 <paulbrooks646@gmail.com>
2021-03-23 13:44:17 -0600

    index.js

--- src/index.js
+++ src/index.js
@@ -3,15 +3,19 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { HashRouter, BrowserRouter } from "react-router-dom"
+import { HashRouter, BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux"
+import store from './redux/store'
 const Router =
   process.env.NODE_ENV === "development" ? HashRouter : BrowserRouter;
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
   </React.StrictMode>,
   document.getElementById("root")
 );
