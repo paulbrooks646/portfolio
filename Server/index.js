ed83e7a15a8facb06a81826b30a4bd27764d1396
paulbrooks646 <paulbrooks646@gmail.com>
2021-03-23 13:10:32 -0600

    index.js

--- Server/index.js
+++ Server/index.js
@@ -1,12 +1,23 @@
 require("dotenv").config({ path: __dirname + "/../.env" });
 const express = require("express");
-
+const massive = require("massive")
+const session = require("express-session")
+const controller = require("./controller")
 const path = require("path");
-const { SERVER_PORT} = process.env;
+const { SERVER_PORT, SESSION_SECRET, CONNECTION_STRING} = process.env;
  
 const app = express();
 
 app.use(express.json());
+app.use(
+  session({
+    resave: false,
+
+    saveUninitialized: true,
+    cookie: { maxAge: 1000 * 60 * 60 * 24 * 30 },
+    secret: SESSION_SECRET,
+  })
+);
 
 app.use(express.static(__dirname + "/../build"));
 
@@ -14,4 +25,16 @@ app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "../build/index.html"));
 });
 
-app.listen(SERVER_PORT, () => console.log(`Running on port ${SERVER_PORT}`));
+massive({
+  connectionString: CONNECTION_STRING,
+  ssl: { rejectUnauthorized: false },
+})
+  .then((db) => {
+    app.set("db", db);
+    console.log("DB connected");
+    app.listen(SERVER_PORT, () =>
+      console.log(`Running on port ${SERVER_PORT}`)
+    );
+  })
+  .catch((err) => console.log(err));
+
