17601194ae8b6fc60cdc115050a8b7d0071184c4
paulbrooks646 <paulbrooks646@gmail.com>
2021-03-24 15:07:56 -0600

    logout functionality

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,9 +1,34 @@
-import React from "react"
+import React, { useEffect } from "react"
+import { Link, withRouter } from "react-router-dom";
+import { connect } from "react-redux"
+import { logoutUser, getUser } from "../../redux/userReducer"
+import axios from "axios"
+import "./Nav.scss"
 
-export default function Nav(props) {
+function Nav(props) {
+
+useEffect(() => {
+  getUser();
+}, []);
+
+const logout = () => {
+  axios.delete("/api/logout").then(() => {
+    props.logoutUser();
+    props.history.push("/Auth");
+  });
+};
+
+
     return (
-        <div>
-
+        <div className="nav-main">
+            <h2 className="nav-welcome">Welcome </h2>
+            <button className="nav-logout" onClick={logout}>Logout</button>
         </div>
     )
 }
+
+const mapStateToProps = (reduxState) => reduxState;
+
+export default withRouter(
+  connect(mapStateToProps, { logoutUser, getUser })(Nav)
+);
