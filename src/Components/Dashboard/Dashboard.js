fe18ac8d50198ce888bcc58aa829533a57fd268b
paulbrooks646 <paulbrooks646@gmail.com>
2021-03-26 16:01:01 -0600

    charcter

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -28,6 +28,37 @@ function Dashboard(props) {
           <div className="dashboard-bottom-right"></div>
         </div>
       </div>
+      <div className="character">
+        <div className="face">
+          <div className="eyes">
+            <div className="eye"></div>
+            <div className="eye"></div>
+          </div>
+          <div className="nose"></div>
+          <div className="mouth"></div>
+        </div>
+        <div className="body">
+          <div className="neck"></div>
+          <div className="arms">
+            <div className="left-arm">
+              <div className="hand"></div>
+            </div>
+            <div className="right-arm">
+              <div className="hand"></div>
+            </div>
+          </div>
+          <div className="torso"></div>
+          <div className="legs">
+            <div className="left-leg">
+              <div className="foot"></div>
+            </div>
+            <div className="right-leg">
+              <div className="foot"></div>
+            </div>
+          </div>
+          <h3>{props.user.user.name}</h3>
+        </div>
+      </div>
     </div>
   );
 }
