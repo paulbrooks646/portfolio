b3011a0049bbb293ba977b07c3c7d7eea4f8dbc0
paulbrooks646 <paulbrooks646@gmail.com>
2021-03-31 15:02:58 -0600

    swamp

--- src/Components/Swamp/Swamp.js
+++ src/Components/Swamp/Swamp.js
@@ -11,27 +11,27 @@ import ArrowBack from "@material-ui/icons/ArrowBack";
 import ArrowUpward from "@material-ui/icons/ArrowUpward";
 
 function Swamp(props) {
+  const [left, setLeft] = useState(false);
+
+  const toggleLeft = () => {
+    setLeft(!left);
+    props.history.push("/Forest");
+  };
+
   return (
     <div className="swamp-main">
       <Nav />
       <div className="swamp-body">
         <div className="swamp-top">
           <div className="swamp-top-left"></div>
-          <div className="swamp-top-middle">
-            <div className="swamp-town">
-              <ArrowUpward />
-              <h2>Nest</h2>
-            </div>
-          </div>
+          <div className="swamp-top-middle"></div>
           <div className="swamp-top-right"></div>
         </div>
         <div className="swamp-middle">
-          <div className="swamp-middle-left"></div>
-          <div className="swamp-middle-middle"></div>
-          <div className="swamp-middle-right">
-            <div className="swamp-entrance">
-              <h2>Entrance</h2>
-              <ArrowForward />
+          <div className="swamp-middle-left">
+            <div className="swamp-forest" onClick={toggleLeft}>
+              <ArrowBack />
+              <h2>Forest</h2>
             </div>
             <div className="character">
               <div className="face">
@@ -65,15 +65,14 @@ function Swamp(props) {
               </div>
             </div>
           </div>
+          <div className="swamp-middle-middle"></div>
+          <div className="swamp-middle-right">
+            <div className="goblin-open"></div>
+          </div>
         </div>
         <div className="swamp-bottom">
           <div className="swamp-bottom-left"></div>
-          <div className="swamp-bottom-middle">
-            <div className="swamp-crag">
-              <h2>Crag</h2>
-              <ArrowDownward />
-            </div>
-          </div>
+          <div className="swamp-bottom-middle"></div>
           <div className="swamp-bottom-right"></div>
         </div>
       </div>
