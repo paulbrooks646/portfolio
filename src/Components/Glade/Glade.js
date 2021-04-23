1a02a0eb0fd074ee9aa05b0ed6050d8960017545
paulbrooks646 <paulbrooks646@gmail.com>
2021-04-23 14:01:01 -0600

    glade animation

--- src/Components/Glade/Glade.js
+++ src/Components/Glade/Glade.js
@@ -15,84 +15,75 @@ import ArrowUpward from "@material-ui/icons/ArrowUpward"
 import { getInventory } from "../../redux/inventoryReducer";
 
 function Glade(props) {
-  const [right, setRight] = useState(false);
-  const [up, setUp] = useState(false);
-  const [failure, setFailure] = useState(false);
-  const [coinSuccess, setCoinSuccess] = useState(false);
-  const [ribbonSuccess, setRibbonSuccess] = useState(false);
-  const [featherSuccess, setFeatherSuccess] = useState(false);
-  const [griffin, setGriffin] = useState(false);
+ const [answerOne, setAnswerOne] = useState(false);
+ const [answerTwo, setAnswerTwo] = useState(false);
+ const [answerThree, setAnswerThree] = useState(false);
+ const [answerFour, setAnswerFour] = useState(false);
+ const [answerFive, setAnswerFive] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
+  const [upCharacter, setUpCharacter] = useState(false)
+  const [rightCharacter, setRightCharacter] = useState(false)
+  const [rightUp, setRightUp] = useState(false)
+  const [rightRight, setRightRight] = useState(false)
+  const [upUp, setUpUp] = useState(false)
+  const [upRight, setUpRight] = useState(false)
 
-  useEffect(() => {
-    axios.get("/api/glade").then((res) => {
-      props.getGlade(res.data[0]);
-      setIsLoading(false);
-    });
-  }, []);
+ useEffect(() => {
+   // if (!props.user.user.newgame) {
+   //   setNewgameCard(false);
+
+   // }
+   axios.get("/api/nest").then((res) => {
+     props.getGlade(res.data[0]);
+
+     if (props.user.user.last === "valley") {
+       setRightCharacter(true);
+     } else if (props.user.user.last === "maze") {
+       setUpCharacter(true);
+     }
+     setIsLoading(false);
+   });
+ }, []);
 
   const toggleRight = () => {
-    setRight(!right);
-    props.history.push("/Valley");
+    axios.post("/api/changeLast", { last: "glade" }).then((res) => {
+      props.getUser(res.data).then(() => {
+        props.history.push("/Valley");
+      });
+    });
   };
 
    const toggleUp = () => {
-     setUp(!up);
-     props.history.push("/Maze");
+     axios.post("/api/changeLast", { last: "glade" }).then((res) => {
+       props.getUser(res.data).then(() => {
+         props.history.push("/Maze");
+       });
+     });
    };
 
-  const toggleFirst = () => {
-    axios.post("/api/gladeFirst").then((res) => {
-      props.getGlade(res.data[0]);
-    });
-  };
 
-  const toggleCoin = () => {
-    if (props.glade.glade.rope_used) {
-      axios.post("/api/coin").then((res) => {
-        props.getUser(res.data);
-        axios.post("/api/gladeCoin").then((res) => {
-          props.getGlade(res.data[0]);
-          setCoinSuccess(true);
-        });
-      });
-    } else {
-      setGriffin(true);
+
+  const toggleGoRight = () => {
+    if (props.user.user.last === "valley") {
+      setRightCharacter(false);
+      setRightRight(true);
+    } else if (props.user.user.last === "maze") {
+      setUpCharacter(false);
+      setUpRight(true);
     }
   };
 
-  const toggleRibbon = () => {
-    if (props.glade.glade.rope_used) {
-      axios.post("/api/ribbon").then((res) => {
-        props.getInventory(res.data);
-        axios.get("/api/glade").then((res) => {
-          props.getGlade(res.data[0]);
-          setRibbonSuccess(true);
-        });
-      });
-    } else {
-      setGriffin(true);
-    }
-  };
+const toggleGoUp = () => {
+   if (props.user.user.last === "valley") {
+    setRightCharacter(false);
+    setRightUp(true);
+  } else if (props.user.user.last === "maze") {
+    setUpCharacter(false);
+    setUpUp(true);
+  }
+};
 
-  const toggleFeather = () => {
-    if (props.glade.glade.rope_used) {
-      axios.post("/api/feather").then((res) => {
-        props.getInventory(res.data);
-        axios.get("/api/glade").then((res) => {
-          props.getGlade(res.data[0]);
-          setFeatherSuccess(true);
-        });
-      });
-    } else {
-      setGriffin(true);
-    }
-  };
-
-  const toggleAnimationEnd = () => {
-    setGriffin(false);
-    setFailure(true);
-  };
+  
 
   return isLoading ? (
     <Loading />
@@ -103,18 +94,57 @@ function Glade(props) {
         <div className="glade-top">
           <div className="glade-top-left"></div>
           <div className="glade-top-middle">
-            <div className="glade-maze" onClick={toggleUp}>
+            <div className="glade-maze" onClick={toggleGoUp}>
               <ArrowUpward />
               <h2>Maze</h2>
+            </div>
+            <div
+              className={`${
+                upCharacter ? "character-up" : "character-up-closed"
+              }`}
+            >
+              <Character />
+            </div>
+            <div
+              className={`${upUp ? "up-up" : "up-up-closed"}`}
+              onAnimationEnd={toggleUp}
+            >
+              <Character />
+            </div>
+            <div
+              className={`${upRight ? "up-right" : "up-right-closed"}`}
+              onAnimationEnd={toggleRight}
+            >
+              <Character />
             </div>
           </div>
           <div className="glade-top-right"></div>
         </div>
         <div className="glade-middle">
-          <div className="glade-middle-left"><div className="unicorn"></div></div>
+          <div className="glade-middle-left">
+            <div className="unicorn"></div>
+          </div>
           <div className="glade-middle-right">
-            <Character />
-            <div className="glade-valley" onClick={toggleRight}>
+            <div
+              className={`${
+                rightCharacter ? "character-right" : "character-right-closed"
+              }`}
+            >
+              <Character />
+            </div>
+            <div
+              className={`${rightUp ? "right-up" : "right-up-closed"}`}
+              onAnimationEnd={toggleUp}
+            >
+              <Character />
+            </div>
+            <div
+              className={`${rightRight ? "right-right" : "right-right-closed"}`}
+              onAnimationEnd={toggleRight}
+            >
+              <Character />
+            </div>
+            <div className="glade-valley" onClick={toggleGoRight}>
               <h2>Valley</h2>
               <ArrowForward />
             </div>
@@ -126,119 +156,7 @@ function Glade(props) {
           <div className="glade-bottom-right"></div>
         </div>
       </div>
-      <Card
-        className={`${
-          props.glade.glade.first_time ? "answer-card" : "answer-card-closed"
-        }`}
-      >
-        <Typography
-          variant="h6"
-          color="secondary"
-          className="answer-card-description"
-        >
-          You climb the steep cliff. Up ahead you see the massive Griffin's
-          glade. You look around tenatively for the owner of the glade.
-        </Typography>
-        <Button
-          onClick={toggleFirst}
-          className="forest-card-button"
-          variant="contained"
-          color="primary"
-        >
-          CLOSE
-        </Button>
-      </Card>
-      <Card className={`${failure ? "answer-card" : "answer-card-closed"}`}>
-        <Typography
-          variant="h6"
-          color="secondary"
-          className="answer-card-description"
-        >
-          Are you serious? Their is a ravenous wolf blocking the path.
-        </Typography>
-        <Button
-          onClick={() => setFailure(false)}
-          className="forest-card-button"
-          variant="contained"
-          color="primary"
-        >
-          CLOSE
-        </Button>
-      </Card>
-      <Card className={`${coinSuccess ? "answer-card" : "answer-card-closed"}`}>
-        <Typography
-          variant="h6"
-          color="secondary"
-          className="answer-card-description"
-        >
-          You pick up the shiny gold coin.
-        </Typography>
-        <Button
-          onClick={() => setCoinSuccess(false)}
-          className="forest-card-button"
-          variant="contained"
-          color="primary"
-        >
-          CLOSE
-        </Button>
-      </Card>
-      <Card
-        className={`${ribbonSuccess ? "answer-card" : "answer-card-closed"}`}
-      >
-        <Typography
-          variant="h6"
-          color="secondary"
-          className="answer-card-description"
-        >
-          You pick up the beautiful blue ribbon.
-        </Typography>
-        <Button
-          onClick={() => setRibbonSuccess(false)}
-          className="forest-card-button"
-          variant="contained"
-          color="primary"
-        >
-          CLOSE
-        </Button>
-      </Card>
-      <Card
-        className={`${featherSuccess ? "answer-card" : "answer-card-closed"}`}
-      >
-        <Typography
-          variant="h6"
-          color="secondary"
-          className="answer-card-description"
-        >
-          You pick up the large griffin feather.
-        </Typography>
-        <Button
-          onClick={() => setFeatherSuccess(false)}
-          className="forest-card-button"
-          variant="contained"
-          color="primary"
-        >
-          CLOSE
-        </Button>
-      </Card>
-      <Card className={`${failure ? "answer-card" : "answer-card-closed"}`}>
-        <Typography
-          variant="h6"
-          color="secondary"
-          className="answer-card-description"
-        >
-          A huge Griffin swoops out of the air. You barely manage to dodge its
-          attack. You need to find something to help you climb to the glade more
-          quickly.
-        </Typography>
-        <Button
-          onClick={() => setFailure(false)}
-          className="forest-card-button"
-          variant="contained"
-          color="primary"
-        >
-          CLOSE
-        </Button>
-      </Card>
+     
     </div>
   );
 }
